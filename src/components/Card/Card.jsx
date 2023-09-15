ecbeea0bc6dde6b2c5a518af0e47b2342d854cce
mrromanmiah <mrromanmiah@gmail.com>
2023-09-15 18:01:12 +0600

    props transfered to the cart section

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,44 +2,62 @@
 import { useEffect, useState } from 'react';
 import { FiDollarSign } from 'react-icons/fi';
 import { HiOutlineBookOpen } from 'react-icons/hi2';
+import Cart from '../Cart/Cart';
 
 const Card = () => {
-const [cards, setCards] = useState([])
+const [cards, setCards] = useState([]);
+const [course, setCourse] = useState([]);
 
     useEffect(() => {
 fetch('./course.json')
 .then(res=>res.json())
 .then((data) => setCards(data))
     },[]);
-console.log(cards);
+
+const handleSelectCourse = (card) => {
+    setCourse([...course, card]);
+
+}
+
+
+
+
     return (
-        <div className='w-3/4 grid grid-cols-3 gap-5 mx-14 mt-8'>
+      <div className='flex'>
+
+<div className='w-3/4 grid grid-cols-3 gap-5 mx-14 mt-8'>
             {
-                cards.map((card) => (<div key={card.id} className='bg-white items-center space-y-3 p-4 rounded-xl'>
+                cards.map((card) => (<div key={card.id} className='bg-white items-center space-y-3 p-3 rounded-xl'>
                 
                 <img className='flex justify-center' src={card.cover} alt="" />
-                <h3 className='text-[#1C1B1B] text-base font-semibold'>{card.title}</h3>
-                <p className='text-[#1c1b1b99] text-sm font-normal'>{card.description}</p>
+                <h3 className='text-[#1C1B1B] text-sm font-semibold'>{card.title}</h3>
+                <p className='text-[#1c1b1b99] text-xs font-normal'>{card.description}</p>
                 
                 <div>
-                <div className='flex items-center gap-4 text-[#1c1b1b99] text-sm font-semibold'>
+                <div className='flex items-center gap-4 text-[#1c1b1b99] text-xs font-semibold'>
                 <div className='flex items-center gap-3'>
-                    <p><FiDollarSign></FiDollarSign></p>
+                    <p className='text-lg text-[#1C1B1B]'><FiDollarSign></FiDollarSign></p>
                     <p>Price : {card.price}</p>
                 </div>
                 <div className='flex items-center gap-3'>
-                    <p><HiOutlineBookOpen></HiOutlineBookOpen></p>
+                    <p className='text-lg text-[#1C1B1B]'><HiOutlineBookOpen></HiOutlineBookOpen></p>
                     <p>Credit : {card.credit}hr</p>
                 </div>
                 </div>
                 
-                    <button className='bg-[#2F80ED] text-white rounded-lg w-full py-2 text-lg font-semibold mt-4'>Select</button>
+                    <button onClick={()=>handleSelectCourse(card)} className='bg-[#2F80ED] text-white rounded-lg w-full py-2 text-sm font-semibold mt-4'>Select</button>
                 
                 </div>
                 
             </div>))
             }
         </div>
+
+        <div className='w-1/4 mt-8'>
+            <Cart course={course}></Cart>
+        </div>
+
+      </div>
     );
 };
 
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,10 @@
 
 
-const Cart = () => {
+const Cart = (props) => {
+    console.log(props);
     return (
         <div>
-            
+            <h1>Roman</h1>
         </div>
     );
 };
