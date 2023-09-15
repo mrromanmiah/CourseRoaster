12dd01a600c84e16030f151e49cfef4f345de2f5
mrromanmiah <mrromanmiah@gmail.com>
2023-09-15 22:02:09 +0600

    cart styles are added; now alerts to be replaced with react toast

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,83 +5,84 @@ import { HiOutlineBookOpen } from 'react-icons/hi2';
 import Cart from '../Cart/Cart';
 
 const Card = () => {
-const [cards, setCards] = useState([]);
-const [course, setCourse] = useState([]);
-const [remainingCredit, setRemainingCredit] = useState(20);
-const [creditTaken, setCreditTaken] = useState(0);
+    const [cards, setCards] = useState([]);
+    const [course, setCourse] = useState([]);
+    const [remainingCredit, setRemainingCredit] = useState(20);
+    const [creditTaken, setCreditTaken] = useState(0);
 
     useEffect(() => {
-fetch('./course.json')
-.then(res=>res.json())
-.then((data) => setCards(data));
-    },[]);
+        fetch('./course.json')
+            .then(res => res.json())
+            .then((data) => setCards(data));
+    }, []);
 
-const handleSelectCourse = (card) => {
-    
+    const handleSelectCourse = (card) => {
 
-    const isSelected = course.find((selected) => selected.id === card.id);
-    let credits = card.credit;
-    if (isSelected) {
-       return alert('already selected');
-    } else {
 
-        course.forEach((selected) => {
-            credits += selected.credit;
-        });
-        const remainingCredits = 20 - credits;
-        if (credits > 20) {
-           return alert('all credits taken')
-        }else{
-            setRemainingCredit(remainingCredits);
-            setCreditTaken(credits);
-            setCourse([...course, card]);
+        const isSelected = course.find((selected) => selected.id === card.id);
+        let credits = card.credit;
+        if (isSelected) {
+            return alert('already selected');
+        } else {
+
+            course.forEach((selected) => {
+                credits += selected.credit;
+            });
+            const remainingCredits = 20 - credits;
+            if (credits > 20) {
+                return alert('all credits taken')
+            } else {
+
+                setRemainingCredit(remainingCredits);
+                setCreditTaken(credits);
+                setCourse([...course, card]);
+            }
+
+
         }
 
-        
-    }
 
-    
-};
+    };
 
 
 
 
     return (
-      <div className='flex'>
+        <div className='flex gap-5'>
 
-<div className='w-3/4 grid grid-cols-3 gap-5 mx-14 mt-8'>
-            {
-                cards.map((card) => (<div key={card.id} className='bg-white items-center space-y-3 p-3 rounded-xl'>
-                
-                <img className='flex justify-center' src={card.cover} alt="" />
-                <h3 className='text-[#1C1B1B] text-sm font-semibold'>{card.title}</h3>
-                <p className='text-[#1c1b1b99] text-xs font-normal'>{card.description}</p>
-                
-                <div>
-                <div className='flex items-center gap-4 text-[#1c1b1b99] text-xs font-semibold'>
-                <div className='flex items-center gap-3'>
-                    <p className='text-lg text-[#1C1B1B]'><FiDollarSign></FiDollarSign></p>
-                    <p>Price : {card.price}</p>
-                </div>
-                <div className='flex items-center gap-3'>
-                    <p className='text-lg text-[#1C1B1B]'><HiOutlineBookOpen></HiOutlineBookOpen></p>
-                    <p>Credit : {card.credit}hr</p>
-                </div>
-                </div>
-                
-                    <button onClick={()=>handleSelectCourse(card)} className='bg-[#2F80ED] text-white rounded-lg w-full py-2 text-sm font-semibold mt-4'>Select</button>
-                
-                </div>
-                
-            </div>))
-            }
+            <div className='w-3/4 grid grid-cols-3 gap-5 ml-12 mt-8'>
+                {
+                    cards.map((card) => (<div key={card.id} className='bg-white items-center space-y-3 p-3 rounded-xl'>
+
+                        <img className='flex justify-center' src={card.cover} alt="" />
+                        <h3 className='text-[#1C1B1B] text-sm font-semibold'>{card.title}</h3>
+                        <p className='text-[#1c1b1b99] text-xs font-normal'>{card.description}</p>
+
+                        <div>
+                            <div className='flex items-center gap-4 text-[#1c1b1b99] text-xs font-semibold'>
+                                <div className='flex items-center gap-3'>
+                                    <p className='text-lg text-[#1C1B1B]'><FiDollarSign></FiDollarSign></p>
+                                    <p>Price : {card.price}</p>
+                                </div>
+                                <div className='flex items-center gap-3'>
+                                    <p className='text-lg text-[#1C1B1B]'><HiOutlineBookOpen></HiOutlineBookOpen></p>
+                                    <p>Credit : {card.credit}hr</p>
+                                </div>
+                            </div>
+
+                            <button onClick={() => handleSelectCourse(card)} className='bg-[#2F80ED] text-white rounded-lg w-full py-2 text-sm font-semibold mt-4'>Select</button>
+
+                        </div>
+
+                    </div>))
+                }
+            </div>
+
+            <div className='w-1/4 mt-8'>
+                <Cart course={course} remainingCredit={remainingCredit} creditTaken={creditTaken}></Cart>
+            </div>
+
         </div>
-
-        <div className='w-1/4 mt-8'>
-            <Cart course={course} remainingCredit={remainingCredit} creditTaken={creditTaken}></Cart>
-        </div>
-
-      </div>
     );
 };
 
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,14 +5,19 @@ const Cart = ({ course, remainingCredit, creditTaken }) => {
 
 
     return (
-        <div>
-            <h3>Credit Hour Remaining {remainingCredit} hr</h3>
-            <ol>
+        <div className="bg-white p-5 mr-12 rounded-xl">
+            <h3 className="text-sm font-bold text-[#2F80ED] mb-3 mt-1">Credit Hour Remaining {remainingCredit} hr</h3>
+            <hr />
+            <h3 className="text-[#1C1B1B] text-base font-bold mt-3 mb-4">Course Name</h3>
+            <ol className="text-[#1c1b1b99] text-xs mb-5">
                 {course.map((card, index) => (
-                    <li key={index}>{index + 1}. {card.title}</li>
+                    <li className="mb-2" key={index}>{index + 1} {card.title}</li>
                 ))}
             </ol>
-            <h3>Total Credit Hour : {creditTaken}</h3>
+            <hr />
+            <h3 className="py-3 text-[#1c1b1bcc] text-sm font-medium">Total Credit Hour : {creditTaken}</h3>
+            <hr />
+            <h3 className="pt-3 text-[#1c1b1bcc] text-sm font-semibold">Total Price : 48000 USD</h3>
         </div>
     );
 };
